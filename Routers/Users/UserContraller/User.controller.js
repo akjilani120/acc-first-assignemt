7a3619f317820ca37548947149268c406a278363
unknown <abdulkaderjilani120@gmail.com>
2022-09-03 23:56:16 +0600

    add post method

--- Routers/Users/UserContraller/User.controller.js
+++ Routers/Users/UserContraller/User.controller.js
@@ -1,11 +1,22 @@
+const fs = require("fs")
+const data = fs.readFileSync("user.json")
+const parseData = JSON.parse(data)
+
 module.exports.RandomUser =(req , res) =>{
-  res.send("random rount is ok")
+    const randomNumber = Math.ceil(Math.random() * 8) 
+    const showData = parseData.find( d => d.id == randomNumber)      
+    res.send(showData)  
+     
 }
 module.exports.AllUser =(req , res) =>{
-res.send("all user ready")
+res.send(parseData)
 }
 module.exports.saveUser =(req , res) =>{
-res.send("Random save user")
+const users = req.body 
+// const saveData = parseData.find(p => (p.id && p.gender && p.name && p.Contact && p.address && p.photoUrl ) !=(users.id && users.gender && users.name && users.Contact && users.address && users.photoUrl))
+// console.log(saveData)
+parseData.push(users)
+res.send(parseData)
 }
 module.exports.updateUser =(req , res) =>{
 res.send("Random  patch update  user")
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const router = require('./Routers/Users/Random/RandomUser.rounter');
 
 const app = express()
 const port = process.env.PORT || 5000
-
+app.use(express.json())
 app.use("/user", router)
 app.get('/', (req, res)=> {
   res.send('Hello World!')
