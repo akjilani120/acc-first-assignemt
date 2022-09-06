8de472818ee0fbe2c5c8868e2bb2625658ca3c7c
unknown <abdulkaderjilani120@gmail.com>
2022-09-07 00:19:58 +0600

    add delete method

--- Routers/Users/Random/RandomUser.rounter.js
+++ Routers/Users/Random/RandomUser.rounter.js
@@ -7,7 +7,7 @@ const UserController = require("../UserContraller/User.controller.js")
  router.route('/all').get(UserController.AllUser)
  router.route('/save').post(UserController.saveUser)
  router.route('/update/:id').patch(UserController.updateUser)
- router.route('/bulk-update').patch(UserController.bulkUpdateUser)
- router.route('/delete').delete(UserController.deleteUser)
+ router.route('/bulk-update/:id').patch(UserController.bulkUpdateUser)
+ router.route('/delete/:id').delete(UserController.deleteUser)
 
  module.exports=router
--- Routers/Users/UserContraller/User.controller.js
+++ Routers/Users/UserContraller/User.controller.js
@@ -1,4 +1,5 @@
 const fs = require("fs")
+const { use } = require("../Random/RandomUser.rounter")
 const data = fs.readFileSync("user.json")
 const parseData = JSON.parse(data)
 
@@ -18,15 +19,28 @@ res.send(parseData)
 }
 module.exports.updateUser =(req , res) =>{
 const {id} = req.params
-let newUpdate =parseData.find(data => data.id == id)
+let newUpdate = parseData.find(data => data.id == id)
 newUpdate.id= id
 newUpdate.name = req.body.name
 console.log(newUpdate)
 res.send(newUpdate)
 }
 module.exports.bulkUpdateUser =(req , res) =>{
-res.send("Random  patch bulk update  user")
+ const {id}=req.params
+ const user = req.body
+ const {name , gender, photoUrl , Contact} = user
+ let newUpdate = parseData.find(data => data.id == id)
+ newUpdate.id = id
+ newUpdate.name= name
+ newUpdate.gender=gender
+ newUpdate.photoUrl = photoUrl
+ newUpdate.Contact = Contact
+ console.log(newUpdate)
+res.send(newUpdate)
 }
 module.exports.deleteUser =(req , res) =>{
-res.send("Random delete user")
+const {id} = req.params
+const deleteData = parseData.filter(data => data.id != id)
+console.log(deleteData)
+res.send(deleteData)
 }
