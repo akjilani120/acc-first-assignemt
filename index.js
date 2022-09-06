21e2922791d59d13b52e04b4e2786c4b971152bc
unknown <abdulkaderjilani120@gmail.com>
2022-09-07 01:31:53 +0600

    add cors

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const http = require('http');
 
 const express = require('express');
 const router = require('./Routers/Users/Random/RandomUser.rounter');
-
+const cors = require("cors")
 const app = express()
 const port = process.env.PORT || 5000
 app.use(express.json())
+app.use(cors())
 app.use("/user", router)
 app.get('/', (req, res)=> {
   res.send('Acc First Assignment!')
